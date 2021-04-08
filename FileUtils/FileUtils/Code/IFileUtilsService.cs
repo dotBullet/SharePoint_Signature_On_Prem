6d07d6a55bdef25074c0c70f2054d13fb048adf0
mariusmoroiu98 <mariusmoroiu98@MVP.SP>
2021-04-08 16:13:39 +0000

    Adaugare WebService pentru SharePoint

---
+++ FileUtils/FileUtils/Code/IFileUtilsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUtils.Code
+{
+    [ServiceContract]
+    public interface IFileUtilsService
+    {
+
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest, UriTemplate = "/SignPDF/{fileID}/{listID}/{webID}/{siteID}/{code}")]
+        Stream SignPDF(string fileID, string listID, string webID, string siteID, string code);
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest, UriTemplate = "/GetAccessToken/{code}")]
+        Stream GetAccessToken(string code);
+      
+
+    }
+}
